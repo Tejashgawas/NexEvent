600830958cfb485540fec1e944b4f635579d5205
Tejash Gawas <gawastejash@gmail.com>
2025-05-03 22:52:59 +0530

    Update app.py

--- AI_rec_model/app.py
+++ AI_rec_model/app.py
@@ -6,6 +6,9 @@ from math import radians, cos, sin, asin, sqrt
 from difflib import get_close_matches
 from flask import Flask, request, jsonify
 from geopy.geocoders import Nominatim
+import warnings
+warnings.filterwarnings("ignore", message="X does not have valid feature names")
+
 
 # Initialize Flask app
 app = Flask(__name__)
